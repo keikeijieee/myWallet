0067f11f4b7721555cf719c5a6eb65edd5953ca0
anreh <anurehka@gmail.com>
2019-12-09 14:07:14 +0800

    Initial commit

---
+++ app/src/main/java/com/example/mywallet/InOutLog.kt
@@ -0,0 +1,16 @@
+package com.example.mywallet
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.sql.Date
+import java.util.*
+
+@Entity(tableName = "in_out_log")
+class InOutLog(
+    @PrimaryKey(autoGenerate = true) val id: Int,
+    val amount : Currency,
+    val type: Int, //1 = income , 0 = expenses
+    val created_at : Date
+
+
+)
